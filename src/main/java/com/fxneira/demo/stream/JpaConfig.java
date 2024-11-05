8bad0890840ee5d6c5dfaf4d6292fb9aa80a9942
Faver Xavier Neira Molina <fxneiram@gmail.com>
2024-11-04 21:03:13 -0500

    jpa config

---
+++ src/main/java/com/fxneira/demo/stream/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.fxneira.demo.stream;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
